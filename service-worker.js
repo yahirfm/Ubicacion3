2fd84f21d94e0d76deeb4f25bb8c8dbf1fb22e47
yahirfm <144049950+yahirfm@users.noreply.github.com>
2024-02-02 09:55:38 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open('mi-cache').then(function (cache) {
+        return cache.addAll([
+          '/',
+          'index.html',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('servicve-worker.js');
+  }
+  
